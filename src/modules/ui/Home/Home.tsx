48798dce4930851b55f703896ac736fa569a68a9
Ankit Sahu <ankit.sahu.as250@gmail.com>
2025-06-04 11:05:17 +0530

    trpc configuration

--- src/modules/ui/Home/Home.tsx
+++ src/modules/ui/Home/Home.tsx
@@ -5,28 +5,17 @@ import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
 import { user } from "../../../../auth-schema";
+import { useTRPC } from "@/app/trpc/client";
+import { useQuery } from "@tanstack/react-query";
 
 export default function Home() {
-  const {data: session} = authClient.useSession();
-   const router = useRouter()
-    if(!session){
-       return (
-        <p>loading...</p>
-       )
-    }
+    const trpc = useTRPC()
+
+    const {data} = useQuery(trpc.hello.queryOptions({text:"Ankit"}))
 
  return(
   <div>
-    <p>Logged in as {session?.user.name}</p>
-    <Button onClick={()=>{ authClient.signOut({
-        fetchOptions:{
-             onSuccess:()=>{
-                 router.push("/sign-in")
-             }
-        }
-    })}} >
-      Sign out
-    </Button>
+   {data?.greeting}
   </div>
   )
 }
