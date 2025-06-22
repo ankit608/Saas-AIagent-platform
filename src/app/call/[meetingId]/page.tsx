a22e692168892fa5bc498cfe2737cb973b910d9c
Ankit Sahu <ankit.sahu.as250@gmail.com>
2025-06-22 17:33:22 +0530

    debug

--- src/app/call/[meetingId]/page.tsx
+++ src/app/call/[meetingId]/page.tsx
@@ -1,3 +1,4 @@
+// In Page.tsx
 import { headers } from "next/headers";
 import { auth } from "@/lib/auth";
 import { trpc } from "@/app/trpc/server";
@@ -5,50 +6,43 @@ import { redirect } from "next/navigation";
 import { getQueryClient } from "@/app/trpc/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { Suspense } from "react";
-import { ErrorBoundary } from "react-error-boundary";
 import CallView from "@/modules/call/ui/views/CallView";
 
-interface Props{
-     params: Promise<{
-        meetingId: string
-     }>
-
-
+interface Props {
+  params: Promise<{
+    meetingId: string;
+  }>;
 }
 
+const Page = async ({ params }: Props) => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  const { meetingId } = await params;
+  console.log(meetingId, "meetingId");
 
- const Page = async({params}:Props)=>{
-    const session = await auth.api.getSession({
-         headers: await headers()
-    })
-    const {meetingId} = await params;
-     console.log(meetingId,"meetingId")
+  if (!session) {
+    redirect("/sign-in");
+  }
 
-    if(!session){
-         redirect("/sign-in");
-
-    }
-
-    const queryClient = getQueryClient();
+  const queryClient = getQueryClient();
+  try {
     await queryClient.prefetchQuery(
-         trpc.meetings.getOne.queryOptions({id:meetingId})
-    )
+      trpc.meetings.getOne.queryOptions({ id: meetingId })
+    );
+  } catch (error) {
+    console.error("Prefetch query failed:", error);
+  }
 
-    console.log("entering to hydration")
- 
-   
-    return(
-         <HydrationBoundary state={dehydrate(queryClient)}>
-             <Suspense fallback={<p>Loading...</p>}>
+  console.log("entering to hydration");
 
-                   <CallView meetingId={meetingId}></CallView> 
-            </Suspense> 
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<div>Loading CallView...</div>}>
+        <CallView meetingId={meetingId} />
+      </Suspense>
+    </HydrationBoundary>
+  );
+};
 
-          
-           
-         </HydrationBoundary>
-    )
-}
-
-
-export default Page
+export default Page;
