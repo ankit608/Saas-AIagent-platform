bad76e2f807cfc244f57d22ed690e5b01ffb7a4f
Ankit Sahu <ankit.sahu.as250@gmail.com>
2025-06-22 15:32:24 +0530

    debug

--- src/app/call/[meetingId]/page.tsx
+++ src/app/call/[meetingId]/page.tsx
@@ -1,58 +1,35 @@
+// 
+
 import { headers } from "next/headers";
 import { auth } from "@/lib/auth";
 import { trpc } from "@/app/trpc/server";
 import { redirect } from "next/navigation";
 import { getQueryClient } from "@/app/trpc/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { Suspense } from "react";
-import { ErrorBoundary } from "react-error-boundary";
 import CallView from "@/modules/call/ui/views/CallView";
 
-// interface Props{
-//      params: Promise<{
-//         meetingId: string
-//      }>
+export default async function Page({ params }: { params: { meetingId: string } }) {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
 
+  const { meetingId } = params;
+  console.log(meetingId, "meetingId");
 
-// }
-interface Props {
-  params: { meetingId: string }
+  if (!session) {
+    redirect("/sign-in");
+  }
+
+  const queryClient = getQueryClient();
+  await queryClient.prefetchQuery(
+    trpc.meetings.getOne.queryOptions({ id: meetingId })
+  );
+
+  console.log("entering to hydration");
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <CallView meetingId={meetingId} />
+    </HydrationBoundary>
+  );
 }
-
- const Page = async({params}:Props)=>{
-    const session = await auth.api.getSession({
-         headers: await headers()
-    })
-    const {meetingId} = params;
-     console.log(meetingId,"meetingId")
-
-    if(!session){
-         redirect("/sign-in");
-
-    }
-
-    const queryClient = getQueryClient();
-    void queryClient.prefetchQuery(
-         trpc.meetings.getOne.queryOptions({id:meetingId})
-    )
-
-    console.log("entering to hydration")
- 
-   
-    return(
-         <HydrationBoundary state={dehydrate(queryClient)}>
-            {/* <Suspense fallback={<p>Loading...</p>}>
-
-                 <ErrorBoundary>
-
-                 </ErrorBoundary>
-            </Suspense> */}
-
-             <CallView meetingId={meetingId}></CallView> 
-           
-         </HydrationBoundary>
-    )
-}
-
-
-export default Page
